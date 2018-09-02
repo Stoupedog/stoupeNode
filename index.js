9539700ea7f93a3678da3dfe2a5b9337b1675c6d
Stoupedog <H.stoupe@gmail.com>
2018-09-02 23:34:40 +1200

    Node stuff

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+let i = 0;
+
+setInterval(function() {
+    if (i < 10) {
+        console.log(i+1);
+        i++;
+    }
+    else {
+        clearInterval(this);
+    }
+}, 500);
